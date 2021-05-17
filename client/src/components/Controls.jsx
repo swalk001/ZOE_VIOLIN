bdb5789c440f2c4fb529d834bb900665e6ea44b1
Steven Walker <swalk001@gmail.com>
2021-05-17 12:45:29 -0400

    fixed sizing issues with music player and animation

--- client/src/components/Controls.jsx
+++ client/src/components/Controls.jsx
@@ -8,34 +8,6 @@ const Controls = ({
     handlePrev,
 }) => {
     return (
-        // <div className="audio2__controls">
-        //     <div className="audio2__controls--choice">
-        //         <ion-icon
-        //             name="play-skip-back-circle-outline"
-        //             onClick={() => handlePrev()}
-        //         ></ion-icon>
-        //     </div>
-        //     <div className="audio2__controls--play">
-        //         {isPlaying === null ? (
-        //             <ion-icon
-        //                 name="play-circle-outline"
-        //                 onClick={() => handlePlay()}
-        //             ></ion-icon>
-        //         ) : (
-        //             <ion-icon
-        //                 name="stop-circle-outline"
-        //                 onClick={() => handleStop()}
-        //             ></ion-icon>
-        //         )}
-        //     </div>
-        //     <div className="audio2__controls--choice">
-        //         <ion-icon
-        //             name="play-skip-forward-circle-outline"
-        //             onClick={() => handleNext()}
-        //         ></ion-icon>
-        //     </div>
-        // </div>
-
         <div className="music__control">
             <div className="music__control--prev">
                 <ion-icon
